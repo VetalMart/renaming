8b4074b1000064f9b50a1407967be22d8e2907a0
VetalMart <7mart77@gmail.com>
2022-06-29 19:38:56 +0300

    first working variant

--- functions.py
+++ functions.py
@@ -2,24 +2,11 @@
 
 def get_text(source):
     """Save text data in list."""
+
     #filling_file = []
     with open(source, "r") as txt_file:
         filling_file = txt_file.readlines()
     return filling_file
-
-def line_in_dict(raw_list):
-    """Put one items into 1 dict."""
-    one_object = {}
-    one_object[raw_list[1]] = "{0}_{1}_{2}_{3}_{4}_{5}_{6}".format(
-                                raw_list[2], 
-                                raw_list[3], 
-                                raw_list[4], 
-                                raw_list[5], 
-                                raw_list[6], 
-                                raw_list[7],
-                                raw_list[8]
-                            )
-    return one_object
 
 def text_in_dict(raw_list):
     """ Make pretty dict, from raw data."""
@@ -42,13 +29,15 @@ def remove_prohibited_signs(raw_list):
         raw_1.append(
             i.replace("/","_").
             replace("\\","_").
-            #replace("|","_").
+            replace("-","").
             replace(":","_").
             replace("*","_").
             replace("?","_").
             replace('"',"_").
             replace("<","_").
-            replace(">","_")
+            replace(">","_").
+            replace(" ","").
+            lstrip()
         )
     return raw_1
 
@@ -58,6 +47,35 @@ def split_sublist(raw_list):
     for i in raw_list:
         list_1.append(i.split("|"))
     return list_1
+
+def line_in_dict(work_dict, raw_list):
+    """Get item form list and put it in dict."""
+    work_dict[raw_list[1]] = "{}_{}_{}_{}_{}_{}_{}".format(
+                                raw_list[2], 
+                                raw_list[3], 
+                                raw_list[4], 
+                                raw_list[5], 
+                                raw_list[6], 
+                                raw_list[7],
+                                raw_list[8]
+                            )
+
+def rename_file(src_file_folder_list, pure_dict):
+    # Rename file in requred way
+    excessive_file = []             # list with excessive file
+    for i in src_file_folder_list:
+        cut_name = i.strip('.pdf')  # number of ask 
+        if cut_name in pure_dict:
+            new_file_name = "{} {}.pdf".format(cut_name, pure_dict[cut_name])
+            # Rename file in proper way
+            os.rename(i, new_file_name)
+        # List of file which doesn't include in source txt file. 
+        else:
+            excessive_file.append(i)
+    # Show on display excessive file
+    for i in excessive_file:
+        print(i)
+
 
 # Exucutes this part if we run this module as executing script
 if __name__ == "__main__":
@@ -75,6 +93,22 @@ if __name__ == "__main__":
     txt_file = input("type source-txt file: > ")
     # Get list with raw text
     raw_info_txt_file_list = get_text(txt_file)
+    #print(raw_info_txt_file_list)
     # Delete prohibited signs from strings.
     semipure_txt = remove_prohibited_signs(raw_info_txt_file_list)
-    # Split prepared list with text
+    #print(semipure_txt)
+    # Splited semipure txt-file
+    splited_txt = split_sublist(semipure_txt)
+    # Put items from semipure-list into 1 dict
+    #print(splited_txt)
+    txt_dict_items = {}
+    for i in splited_txt:
+        # Need remove empty list, and use only full info
+        if i != ['']:
+            line_in_dict(txt_dict_items, i)
+        else:
+            pass
+    # Rename files and create list which not. 
+    rename_file(list_file_in_working_dir, txt_dict_items) 
+    
+    
