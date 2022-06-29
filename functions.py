f19e7c241b02c0ead85c1808b8b2081512246e65
VetalMart <7mart77@gmail.com>
2022-06-29 12:51:20 +0300

    1 address in 1 key-pair value

--- functions.py
+++ functions.py
@@ -7,18 +7,19 @@ def get_text(source):
         filling_file = txt_file.readlines()
     return filling_file
 
-def line_in_dict(line):
-    """Put one line into 1 dict."""
-    one_oject = {
-        "number":"",
-        "data":"",
-        "locality":"",
-        "steet":"",
-        "build":"",
-        "flat":"",
-        "feature":"",
-        "customer":"",
-    }
+def line_in_dict(raw_list):
+    """Put one items into 1 dict."""
+    one_object = {}
+    one_object[raw_list[1]] = "{0}_{1}_{2}_{3}_{4}_{5}_{6}".format(
+                                raw_list[2], 
+                                raw_list[3], 
+                                raw_list[4], 
+                                raw_list[5], 
+                                raw_list[6], 
+                                raw_list[7],
+                                raw_list[8]
+                            )
+    return one_object
 
 def text_in_dict(raw_list):
     """ Make pretty dict, from raw data."""
@@ -51,6 +52,13 @@ def remove_prohibited_signs(raw_list):
         )
     return raw_1
 
+def split_sublist(raw_list):
+    # Split by sublist
+    list_1 = []
+    for i in raw_list:
+        list_1.append(i.split("|"))
+    return list_1
+
 # Exucutes this part if we run this module as executing script
 if __name__ == "__main__":
     import os
@@ -69,3 +77,4 @@ if __name__ == "__main__":
     raw_info_txt_file_list = get_text(txt_file)
     # Delete prohibited signs from strings.
     semipure_txt = remove_prohibited_signs(raw_info_txt_file_list)
+    # Split prepared list with text
